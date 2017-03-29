a6969dc0e3822e0b263cda4e85225ac51f7cd39f
Kai <kai.kikuchi27@gmail.com>
2017-03-29 08:30:33 +0200

    Version 0.9.3

--- src/main/java/net/kaikk/mc/kaiscommons/sponge/KaisCommons.java
+++ src/main/java/net/kaikk/mc/kaiscommons/sponge/KaisCommons.java
@@ -2,7 +2,7 @@ package net.kaikk.mc.kaiscommons.sponge;
 
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id="kaiscommons", name = "Kai's Commons", version = "0.9", description = "")
+@Plugin(id="kaiscommons", name = "Kai's Commons", version = "0.9.3", description = "")
 public class KaisCommons {
 
 }
